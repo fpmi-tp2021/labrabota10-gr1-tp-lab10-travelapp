375705c1f10571673769aa2e6e3d152cc9a84519
Dzmitry <dima.semnovich.01@ickoud.com>
2021-06-03 00:23:36 +0300

    feat: implement card

---
+++ TravelApp/TravelApp/HotelCard.swift
@@ -0,0 +1,20 @@
+//
+//  HotelCard.swift
+//  TravelApp
+//
+//  Created by Dzmitry Semenovich on 2.06.21.
+//
+
+import SwiftUI
+
+struct HotelCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HotelCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HotelCard()
+    }
+}
