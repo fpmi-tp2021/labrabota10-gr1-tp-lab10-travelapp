c1f59047d45583ba788053cb6f9b7eb94a59f34a
Dzmitry <dima.semnovich.01@ickoud.com>
2021-06-02 20:11:42 +0300

    feat: implement JSON decoder and search filters

--- TravelApp/TravelApp/HomeView.swift
+++ TravelApp/TravelApp/HomeView.swift
@@ -9,35 +9,46 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State var text = ""
-
+    @State var city = ""
+    @State var checkIn = Date()
+    @State var checkOut = Date()
+    @State var adults = 1
+    @State var maxPrice = 300
+    @State var minPrice = 150
+    
     var body: some View {
         
-        let hotelsModel = HotelRequest()
-        
         VStack {
-        HStack(alignment: .lastTextBaseline){
-            Button(action: {
-                UserDefaults.standard.set(false, forKey: "status")
-                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
-                
-            }) {
-                
-                Text("Logout")
-            }
-        }
-        
+            
+            /* Logout button
+             VStack(alignment: .leading){
+             Button(action: {
+             UserDefaults.standard.set(false, forKey: "status")
+             NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+             
+             }) {
+             
+             Image("logout").resizable().imageScale(.small).frame(width: 20.0, height: 20.0)
+             }
+             }.frame(width: UIScreen.main.bounds.width - 30, height: 20, alignment: .leading)
+             */
+            
             ZStack(alignment: .trailing) {
                 RoundedRectangle(cornerRadius: 30)
                     .frame(height: 55)
                     .foregroundColor(Color(.secondarySystemBackground))
                     .padding()
                 
-                TextField("Find your travel direction", text: $text)
+                TextField("Find your travel direction", text: $city)
                     .padding(.leading)
                     .offset(x: 12)
                 
-                Button(action: { hotelsModel.getHotels() }, label: {
+                Button(action: {
+                    let hotelsRequest = HotelRequest(city: city, checkIn: checkIn, checkOut: checkOut, adults: adults, maxPrice: maxPrice, minPrice: minPrice)
+                    
+                        hotelsRequest.getHotels()
+                    
+                }, label: {
                     Circle()
                         .frame(width: 45, height: 45)
                         .foregroundColor(Color(.systemTeal))
@@ -47,10 +58,41 @@ struct HomeView: View {
                         .offset(x: -5)
                 })
                 
-            }.padding()
+            }
+            
+            Form{
+                
+                Section(header: Text("Date").fontWeight(.heavy).font(.title3)){
+                    
+                    VStack{
+                        
+                        DatePicker("Check in date", selection: $checkIn, displayedComponents: [.date])
+                    }
+                    VStack{
+                        
+                        DatePicker("Check out date", selection: $checkOut, displayedComponents: [.date])
+                    }
+                    
+                }
+                Section(header: Text("People").fontWeight(.heavy).font(.title3)) {
+                    
+                    
+                    Stepper("Amount of adults  \(adults)", value: $adults, in: 1...6)
+                    
+                }
+                
+                Section(header: Text("Price").fontWeight(.heavy).font(.title3)) {
+                    
+                    Stepper("Maximum price  \(maxPrice)", value: $maxPrice, in: 300...1000, step: 20)
+                    
+                    Stepper("Minimum price  \(minPrice)", value: $minPrice, in: 50...700, step: 20)
+
+                }
+            }
         }
     }
 }
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
