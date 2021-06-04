dc50bcd2b78a96b1842e0bd97fa856cd84b5b1dd
Dzmitry <dima.semnovich.01@ickoud.com>
2021-06-04 11:31:59 +0300

    feat: implement Hotels table view

--- TravelApp/TravelApp/HotelCard.swift
+++ TravelApp/TravelApp/HotelCard.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct HotelCard: View {
+    
+    @State var stars : Int
+    @State var name : String
+    @State var distance : String
+    @State var price : String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+
+           // Image("logo").resizable().frame( width: 150, height: 150, alignment: .center).scaledToFit()
+              //  .imageScale(.small).background(Color.white).cornerRadius(15)
+            
+            VStack{
+            VStack(alignment: .leading) {
+               
+                Text("\(name)").font(.title2).fontWeight(.regular).lineLimit(1).frame(maxWidth: .infinity, alignment: .leading).padding(.bottom, 1)
+                
+                HStack {
+                    ForEach(0..<stars, id: \.self) { item in
+                        Image(systemName: "star.fill").font(.body).foregroundColor(.yellow)
+                    }
+                }.padding(.bottom)
+                
+                
+                Text("From city center \(distance)").font(.body).fontWeight(.light)
+                Text("Price \(price)").font(.body).fontWeight(.light)
+
+
+            }
+            }
+        }.padding(.bottom, 5)
+        
     }
 }
 
 struct HotelCard_Previews: PreviewProvider {
     static var previews: some View {
-        HotelCard()
+        HotelCard(stars: 4, name: "London", distance: "1.5 mile", price: "150$").previewLayout(.sizeThatFits)
     }
 }
